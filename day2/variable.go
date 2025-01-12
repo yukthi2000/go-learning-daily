6aa84e3a1bf4d7ac4d3e4b0fb74af0d37bd3090c
Yukthi <yukthihettiarachchissck@gmail.com>
2025-01-12 09:56:28 +0530

    how to define variables

---
+++ day2/variable.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var name string ="jhon"
+	fmt.Println("Name",name)
+
+	var test="test"
+	fmt.Println("test",test)
+
+	var a,c= 2,3
+	fmt.Println("a,c",a,c)
+
+	var bol= true
+	fmt.Println("boolean",bol)
+
+	const country = "USA"
+	fmt.Println("country",country)
+
+	age:=30
+	fmt.Println("age",age,"Type of Age",)
+
+	
+
+
+
+
+
+}
