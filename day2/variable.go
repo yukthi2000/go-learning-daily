10830678e4409363c8f4c1dfae492aa867261423
Yukthi <yukthihettiarachchissck@gmail.com>
2025-01-12 10:00:42 +0530

    check types of variables

--- day2/variable.go
+++ day2/variable.go
@@ -1,31 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-func main(){
+func main() {
 
-	var name string ="jhon"
-	fmt.Println("Name",name)
+	var name string = "jhon"
+	fmt.Println("Name", name)
 
-	var test="test"
-	fmt.Println("test",test)
+	var test = "test"
+	fmt.Println("test", test)
 
-	var a,c= 2,3
-	fmt.Println("a,c",a,c)
+	var a, c = 2, 3
+	fmt.Println("a,c", a, c)
 
-	var bol= true
-	fmt.Println("boolean",bol)
+	var bol = true
+	fmt.Println("boolean", bol)
 
 	const country = "USA"
-	fmt.Println("country",country)
+	fmt.Println("country", country)
 
-	age:=30
-	fmt.Println("age",age,"Type of Age",)
+	age := 30
+	fmt.Println("age", age, " - Type of Age", reflect.TypeOf(age))
 
-	
-
-
-
-
+	NameStrin := "test string"
+	fmt.Println("NameStrin ", NameStrin, "TYPE - ", reflect.TypeOf(NameStrin))
 
 }
